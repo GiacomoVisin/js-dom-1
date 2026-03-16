300c9d8112d768acf010b87c76c956c12b559b1c
GiacomoVisin <killerboss1150@gmail.com>
2026-03-16 15:51:11 +0100

    adding new image button

--- index.js
+++ index.js
@@ -4,24 +4,50 @@ console.log(lamp);
 const btn = document.getElementById("btn")
 console.log(btn);
 
+const btnOff = document.getElementById("buttonOff")
 
 
 
-
-function turnOnAndOff(){
-    if(lamp.src.includes("white_lamp")){
-       lamp.src = "img/yellow_lamp.png" 
-       btn.textContent= "spegni"
+function turnOnAndOff() {
+    if (lamp.src.includes("white_lamp")) {
+        lamp.src = "img/yellow_lamp.png"
+        btn.textContent = "spegni"
+     
     }
-    else{
-         lamp.src = "img/white_lamp.png" 
-       btn.textContent= "accendi"
-    } 
+    else {
+        lamp.src = "img/white_lamp.png"
+        btn.textContent = "accendi"
+    
+        
+    }
 
 }
 
 
-btn.addEventListener("click",turnOnAndOff)
+
+
+function turnOnAndOff2() {
+    if (lamp.src.includes("white_lamp")) {
+        lamp.src = "img/yellow_lamp.png"
+        btnOff.src ="img/button-off.png"
+        btn.textContent = "spegni"
+     
+    }
+    else {
+        lamp.src = "img/white_lamp.png"
+          btnOff.src ="img/button-on.png"
+        btn.textContent = "accendi"
+    
+        
+    }
+
+}
+
+btnOff.addEventListener("click", turnOnAndOff2)
+
+
+
+
 
 
 

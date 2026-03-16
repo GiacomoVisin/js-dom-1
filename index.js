2e3af0848abe36fe0e2c1a25bd96d787481c6c65
GiacomoVisin <killerboss1150@gmail.com>
2026-03-16 15:23:02 +0100

    fixing js codes and adding the EventListener

--- index.js
+++ index.js
@@ -10,14 +10,17 @@ console.log(btn);
 function turnOnAndOff(){
     if(lamp.src.includes("white_lamp")){
        lamp.src = "img/yellow_lamp.png" 
-       button.textContent= "spegni"
+       btn.textContent= "spegni"
     }
     else{
          lamp.src = "img/white_lamp.png" 
-       button.textContent= "accendi"
+       btn.textContent= "accendi"
     } 
 
 }
+
+
+btn.addEventListener("click",turnOnAndOff)
 
 
 

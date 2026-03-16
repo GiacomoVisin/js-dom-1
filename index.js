6636604ce6e3a6d57b8159c3cfd61f6776a7b036
GiacomoVisin <killerboss1150@gmail.com>
2026-03-16 15:19:35 +0100

    create the function

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ console.log(btn);
 
 
 
+function turnOnAndOff(){
+    if(lamp.src.includes("white_lamp")){
+       lamp.src = "img/yellow_lamp.png" 
+       button.textContent= "spegni"
+    }
+    else{
+         lamp.src = "img/white_lamp.png" 
+       button.textContent= "accendi"
+    } 
+
+}
+
+
+
 
 
 
